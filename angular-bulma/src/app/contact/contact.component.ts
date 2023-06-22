f2d01b57df7da95c73088a1f91672ffe74c66850
minharaza <razamh@mail.uc.edu>
2023-06-22 09:26:24 -0400

    Contact form!
    I'm currently working on the contact form, and specifically trying to figure out what data binding is and how to apply two way data binding to be able to collect data from my form when the user inputs their info. I'm a bit stuck right now because it does not seem to be updating correctly at the moment!

--- angular-bulma/src/app/contact/contact.component.ts
+++ angular-bulma/src/app/contact/contact.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  name = " "
+  email = " "
+  message = " "
+
   
+  constructor() { }
+
+  ngOnInit() {}
+
 
   submitForm(){
     //grabbing all the fields and their values
+    const message = "my name is this.name"
+    alert(message);
   }
 
 }
